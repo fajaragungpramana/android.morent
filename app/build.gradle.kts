8050b832923bc205e378c06f457611f65f31b946
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-02-17 11:31:57 +0700

    Create build variant debug.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,11 +19,15 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
         }
     }
     compileOptions {
