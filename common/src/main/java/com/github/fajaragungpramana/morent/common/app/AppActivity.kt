4b60a909c7326506b0741cd1262ad809669244e2
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-02-17 13:55:06 +0700

    Create base activity and base state contract

---
+++ common/src/main/java/com/github/fajaragungpramana/morent/common/app/AppActivity.kt
@@ -0,0 +1,28 @@
+package com.github.fajaragungpramana.morent.common.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import com.github.fajaragungpramana.morent.common.contract.AppState
+
+abstract class AppActivity<VB : ViewBinding> : AppCompatActivity() {
+
+    private lateinit var _viewBinding: VB
+    val viewBinding: VB
+        get() = _viewBinding
+
+    protected abstract fun onViewBinding(): VB
+
+    protected abstract fun onCreated(savedInstanceState: Bundle?)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!::_viewBinding.isInitialized)
+            _viewBinding = onViewBinding()
+
+        onCreated(savedInstanceState)
+
+        if (this is AppState) onStateObserver()
+    }
+
+}
