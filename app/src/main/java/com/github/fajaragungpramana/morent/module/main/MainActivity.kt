e9411c34de310d969c33c373f61a17bce50c101a
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-02-18 10:41:22 +0700

    Show list house to view.

--- app/src/main/java/com/github/fajaragungpramana/morent/module/main/MainActivity.kt
+++ app/src/main/java/com/github/fajaragungpramana/morent/module/main/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import coil.load
 import coil.transform.CircleCropTransformation
 import com.github.fajaragungpramana.morent.common.app.AppActivity
 import com.github.fajaragungpramana.morent.common.contract.AppState
 import com.github.fajaragungpramana.morent.core.domain.user.model.User
 import com.github.fajaragungpramana.morent.databinding.ActivityMainBinding
+import com.github.fajaragungpramana.morent.module.adapter.HouseAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -19,9 +21,13 @@ class MainActivity : AppActivity<ActivityMainBinding>(), AppState {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private lateinit var houseAdapter: HouseAdapter
+
     override fun onViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
     override fun onCreated(savedInstanceState: Bundle?) {
+        initHouse()
+
         viewModel.setEvent(MainEvent.USER)
         viewModel.setEvent(MainEvent.LIST_HOUSE)
     }
@@ -31,11 +37,19 @@ class MainActivity : AppActivity<ActivityMainBinding>(), AppState {
             viewModel.state.collectLatest {
                 when (it) {
                     is MainState.UserData -> setUserView(it.user)
-                    is MainState.HouseData -> Log.d("FFFF", "${it.listHouse}")
+                    is MainState.HouseData -> houseAdapter.submitList(it.listHouse)
 
                     is MainState.MessageData -> Log.d("FFFF", "")
                 }
             }
+        }
+    }
+
+    private fun initHouse() {
+        houseAdapter = HouseAdapter()
+        viewBinding.apply {
+            rvHouse.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvHouse.adapter = houseAdapter
         }
     }
 
