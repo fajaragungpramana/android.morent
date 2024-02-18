5ea7f09bfff557b033b09f5fad425a66f17839cb
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-02-18 09:51:02 +0700

    Display user data to view.

--- app/src/main/java/com/github/fajaragungpramana/morent/module/main/MainActivity.kt
+++ app/src/main/java/com/github/fajaragungpramana/morent/module/main/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.github.fajaragungpramana.morent.common.app.AppActivity
 import com.github.fajaragungpramana.morent.common.contract.AppState
+import com.github.fajaragungpramana.morent.core.domain.user.model.User
 import com.github.fajaragungpramana.morent.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -27,12 +30,21 @@ class MainActivity : AppActivity<ActivityMainBinding>(), AppState {
         lifecycleScope.launch {
             viewModel.state.collectLatest {
                 when (it) {
-                    is MainState.UserData -> Log.d("FFFF", "${it.user}")
+                    is MainState.UserData -> setUserView(it.user)
                     is MainState.HouseData -> Log.d("FFFF", "${it.listHouse}")
 
                     is MainState.MessageData -> Log.d("FFFF", "")
                 }
             }
+        }
+    }
+
+    private fun setUserView(user: User) {
+        viewBinding.apply {
+            aivUserAvatar.load(user.avatar) {
+                transformations(CircleCropTransformation())
+            }
+            mtvUserName.text = user.name
         }
     }
 
